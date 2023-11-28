af05fc1a32869cf54e54b19439de1ad40aac6289
Orestis Meikopoulos <diktyas1988@gmail.com>
2023-11-28 15:51:50 +0200

    Configure different wait strategy for cosmos db container.

--- tests/FunctionApp.IsolatedDemo.Api.Tests.Integration/NotesFunctionFixture.cs
+++ tests/FunctionApp.IsolatedDemo.Api.Tests.Integration/NotesFunctionFixture.cs
@@ -1,22 +1,33 @@
 ﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Testcontainers.CosmosDb;
 using Xunit;
 
 namespace FunctionApp.IsolatedDemo.Api.Tests.Integration
 {
-    public class NotesFunctionFixture : IAsyncLifetime
+    public class NotesFunctionFixture : IAsyncLifetime, IDisposable
     {
-        private readonly CosmosDbContainer _cosmosDbContainer = new CosmosDbBuilder()
-            .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest")
-            .WithName("Cosmos_DB")
-            .WithPortBinding(8081, 8081)
-            .WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "1")
-            .WithEnvironment("AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE", "127.0.0.1")
-            .WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "false")
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8081))
-            .Build();
+        private readonly IOutputConsumer _outputConsumer = Consume.RedirectStdoutAndStderrToStream(new MemoryStream(), new MemoryStream());
+        private readonly CosmosDbContainer _cosmosDbContainer;
         private readonly NotificationApiServer _notificationApiServer = new();
+
+        public NotesFunctionFixture()
+        {
+            _cosmosDbContainer = new CosmosDbBuilder()
+                .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest")
+                .WithName("Cosmos_DB")
+                .WithExposedPort(8081)
+                .WithPortBinding(8081, 8081)
+                .WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "1")
+                .WithEnvironment("AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE", "127.0.0.1")
+                .WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "false")
+                .WithOutputConsumer(_outputConsumer)
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilMessageIsLogged("Started"))
+                .Build();
+        }
 
         public string GetCosmosDbConnectionString() => _cosmosDbContainer.GetConnectionString();
         public string GetNotificationApiServerUrl() => _notificationApiServer.Url;
@@ -32,6 +43,11 @@ namespace FunctionApp.IsolatedDemo.Api.Tests.Integration
             _notificationApiServer.Start();
             _notificationApiServer.SetupRequestDetails();
             await _cosmosDbContainer.StartAsync();
+        }
+
+        public void Dispose()
+        {
+            _outputConsumer.Dispose();
         }
     }
 }
